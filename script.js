9d0bb545d11825751a6d59e2658ff888109dd532
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-03 15:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function arr(a){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{resolve(a)},3000)
+    })
+}
+
+arr([1,2,3,4]).then((r)=>r)
+	.then(r=>r.filter(i=>i%2==0))
+	.then(r=>r.map(i=>i*2))
+	.then(r=>{document.getElementById("output").innerHTML=r})
